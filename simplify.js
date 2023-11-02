aee2a819ee5407a2af42a3c841788ad6421832ca
Josh Mandel <jmandel@alum.mit.edu>
2023-11-02 16:25:23 -0500

    Update simplify.js

--- simplify.js
+++ simplify.js
@@ -3,7 +3,7 @@ import fhirpath from "fhirpath";
 const expr = "Patient.where(text ~ 'Josh').name.where($this.title.substring(3, 2, 1).startsWith('J')).given"
 const tree = fhirpath.parse(expr);
 console.log(JSON.stringify(tree, null, 2))
-const simpler = simplifyParseTree(tree);
+const simpler = simplify(tree);
 console.log(JSON.stringify(simpler, null, 2))
 
 function isArray(path) {
@@ -12,71 +12,66 @@ function isArray(path) {
   return false;
 }
 
-function simplifyParseTree(tree) {
-  // Helper function to check if the path is an array
-
-  function simplifyNode(node) {
-    // Debug print
-    console.log(`Simplifying node of type: ${node.type}`);
+function simplify(node) {
+  switch (node.type) {
+    case undefined:
+    case 'EntireExpression':
+    case 'ParenthesizedTerm':
+      return simplify(node.children[0]);
+    case 'AndExpression':
+    case 'OrExpression':
+    case 'UnionExpression':
+      return [{fn: node.terminalNodeText, args: node.children.map(simplify)}]
     
-    switch (node.type) {
-      case undefined:
-        return simplifyNode(node.children[0]);
-
-      case 'EntireExpression':
-        return simplifyNode(node.children[0]);
-      
-      case 'InvocationExpression':
-        if (node.terminalNodeText[0] === '.') {
-          const navigationNode = simplifyNode(node.children[1]);
-          const beforeNavigation = simplifyNode(node.children[0]);
-          return beforeNavigation.concat(navigationNode);
-        }
-        break;
-      
-      case 'TermExpression':
-        if (node.children.length === 1) {
-          return simplifyNode(node.children[0]);
-        }
-        break;
-      
-      case 'InvocationTerm':
-        if (node.children.length === 1) {
-          return simplifyNode(node.children[0]);
-        }
-        break;
-      
-      case 'MemberInvocation':
-        if (node.children.length === 1) {
-          return simplifyNode(node.children[0]);
-        }
-        break;
-      
-      case 'Identifier':
-        return [{ "navigation": node.terminalNodeText[0], "array": isArray(node.terminalNodeText[0]) }];
-      
-      case 'FunctionInvocation':
-        const functionName = node.children[0].children[0].terminalNodeText[0];
-        const args = node.children[0].children[1].children.map(param => simplifyNode(param));
-        return [{ "fn": functionName, "args": args }];
-      
-      case 'EqualityExpression':
-        const operator = node.terminalNodeText[0];
-        const leftOperand = simplifyNode(node.children[0]);
-        const rightOperand = simplifyNode(node.children[1]);
-        return [{ "fn": operator, "args": [leftOperand, rightOperand] }];
-      
-      case 'LiteralTerm':
-        const literalValue = node.children[0].terminalNodeText[0].replace(/'/g, '');
-        return [{ "literal": literalValue }];
-      case 'ThisInvocation':
-        return [{ "context": "$this"}]
-      
-      default:
-        console.log(`Unhandled node type: ${node.type}`);
-        return [];
-    }
+    case 'InvocationExpression':
+      if (node.terminalNodeText[0] === '.') {
+        const navigationNode = simplify(node.children[1]);
+        const beforeNavigation = simplify(node.children[0]);
+        return beforeNavigation.concat(navigationNode);
+      }
+      break;
+    
+    case 'TermExpression':
+      if (node.children.length === 1) {
+        return simplify(node.children[0]);
+      }
+      break;
+    
+    case 'InvocationTerm':
+      if (node.children.length === 1) {
+        return simplify(node.children[0]);
+      }
+      break;
+    
+    case 'MemberInvocation':
+      if (node.children.length === 1) {
+        return simplify(node.children[0]);
+      }
+      break;
+    
+    case 'Identifier':
+      return [{ "navigation": node.terminalNodeText[0], "array": isArray(node.terminalNodeText[0]) }];
+    
+    case 'FunctionInvocation':
+      const functionName = node.children[0].children[0].terminalNodeText[0];
+      const args = node.children[0].children[1].children.map(param => simplify(param));
+      return [{ "fn": functionName, "args": args }];
+    
+    case 'InequalityExpression':
+    case 'EqualityExpression':
+      const operator = node.terminalNodeText[0];
+      const leftOperand = simplify(node.children[0]);
+      const rightOperand = simplify(node.children[1]);
+      return [{ "fn": operator, "args": [leftOperand, rightOperand] }];
+    
+    case 'LiteralTerm':
+      const literalValue = node.children[0].terminalNodeText[0].replace(/'/g, '');
+      return [{ "literal": literalValue }];
+    case 'ThisInvocation':
+      return [{ "context": "$this"}]
+    
+    default:
+      console.log(`Unhandled node type: ${node.type}`);
+      return [];
   }
-
-  return simplifyNode(tree);
 }

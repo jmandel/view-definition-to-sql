0516821dabbdef19f8348e6c27611b539a53306e
Josh Mandel <jmandel@alum.mit.edu>
2023-11-08 19:06:03 -0600

    Partial generation with nested cols and selects

--- sql.js
+++ sql.js
@@ -2,25 +2,34 @@ import {fpparse} from './simplify'
 
 let counter = {}
 function unique(n) {
-  counter[n] = counter[n] ? counter[n] + 1 : 0
+  if (counter[n] !== undefined) {
+    counter[n] += 1;
+  } else {
+    counter[n] = 0
+  }
   return n + (counter[n] > 0 ? counter[n] : '')
 }
 
 const opName = {
   '~': 'TILDE',
   '=': 'EQ',
+  '>': 'GREATER',
+  '>=': 'GREATER_EQ',
+  '<': 'LESS',
+  '<=': 'LESS_EQ',
 }
 
 function pathToAst(pathArray, parent = null, nameHint = "p") {
-  console.log(pathArray)
   const ret = []
 
   const incomingSource = parent?.forEach ? parent?.forEach?.path?.at(-1)?.source : parent.source
   // const target = parent?.forEach ? parent?.forEach?.path?.at(-1)?.target : parent.target
   let expr = ''
+  let target
+  let source
   for (const [i, p] of pathArray.entries()) {
-    const source = ret.length === 0 ? incomingSource : ret.at(-1).target
-    const target =  { table: source.table + "_" +(ret.length ? "p":nameHint)+ret.length, column: 'value' };
+    source = ret.length === 0 ? incomingSource : ret.at(-1).target
+    target =  { table: unique((parent.targetPrefix || source.table) + "_" +(nameHint) + (i >= 0 ? `_part${i}`: "")), column: 'value' };
     if (p.literal) {
       ret.push({
         literal: p.literal,
@@ -57,7 +66,7 @@ function pathToAst(pathArray, parent = null, nameHint = "p") {
         type: p.type?.type,
         jsonPath: expr ? `$${expr}` : undefined,
         op: p.fn,
-        args: p.args.map((a, i) => pathToAst(a, { source }, (opName[p.fn] ?? p.fn))),
+        args: p.args.map((a, i) => pathToAst(a, { source }, (opName[p.fn] ?? p.fn)+`_arg${i}`)),
       }
       context.push(fn)
       expr = '';
@@ -65,8 +74,6 @@ function pathToAst(pathArray, parent = null, nameHint = "p") {
   }
 
   if (expr) {
-    const source = ret.length === 0 ? incomingSource : ret.at(-1).target
-    const target =  { table: source.table + '_p'+ret.length+'_literal' + ret.length, column: 'value' };
     ret.push({
       source,
       // target: { table: 'continued-target' },
@@ -87,34 +94,35 @@ function viewDefinitionToQueryAst(viewDefinition) {
   function handleSelect(parent, selectBlock, type = null) {
     let targetType = type
     if (selectBlock.forEach) {
-      const path = fpparse(selectBlock.forEach, type || viewDefinition.resource)
+      const path = fpparse(selectBlock.forEach, type)
       targetType = path.at(-1).type
       parent.forEach = {
-        target: null,
+        target: {table: parent.target.table+"_each"},
         source: parent.source,
-        path: pathToAst(path, parent, "each"),
+        path: pathToAst(path, parent, parent.target.table+"_each"),
       }
-      parent.forEach.target = parent.forEach.path.at(-1).target
+      // parent.forEach.target = parent.forEach.path.at(-1).target
+      parent.forEach.path.at(-1).target = parent.forEach.target 
     }
-    // Handle the column in the select block
-    ;(selectBlock.column || []).forEach((column) => {
-      const path = fpparse(column.path, targetType || viewDefinition.resource)
-      const source = parent?.forEach ? parent?.forEach?.path?.at(-1)?.target : parent.source
+
+    const source = parent?.forEach ? parent?.forEach?.path?.at(-1)?.target : parent.source
+    ;(selectBlock.column || []).forEach((column, i) => {
+      const path = fpparse(column.path, targetType)
       // Append to the selections list in the AST
       parent.column.push({
         name: column.name,
-        source: source,
-        path: pathToAst(path, {source}, "col"),
+        source,
+        path: pathToAst(path, {source, targetPrefix: parent.target.table}, "col"+i),
       })
       parent.column.at(-1).target = parent.column.at(-1).path.at(-1).target
     })
-    // Handle any nested select blocks inside the forEach block
+
     if (selectBlock.select) {
       selectBlock.select.forEach((nestedSelect, i) => {
         const blockTemplate = {
           ...blockTemplateEmpty(),
-          source: parent.forEach ?? parent.source,
-          target: { table: 'result' + `_s${i}` },
+          source,
+          target: { table: parent.target.table+ `_s${i}` },
         }
         handleSelect(blockTemplate, nestedSelect, targetType)
         parent.select.push(blockTemplate)
@@ -125,56 +133,47 @@ function viewDefinitionToQueryAst(viewDefinition) {
   const blockTemplate = {
     ...blockTemplateEmpty(),
     source: { table: viewDefinition.resource, column: 'value' },
+    target: { table: "result"}
   }
-  handleSelect(blockTemplate, viewDefinition)
+  handleSelect(blockTemplate, viewDefinition, viewDefinition.resource)
   return blockTemplate
 }
 
-function queryWhere(args) {
-  return "QUERE" + JSON.stringify(args)
-  const parts = args.slice(args[0].context ? 1 : 0);
-}
-
 function queryPathItemToSql(p, isForEach) {
-  console.log("QP", p)
-  let prerequisites = []
+  let prerequisites = (p.args || []).flatMap(a => a.flatMap(queryPathItemToSql));
+
   if (p.context === "$this") {
-    return [`${p.target.table} as (uselect * from ${p.source.table})`]
+    return prerequisites.concat([`${p.target.table} as (select * from ${p.source.table})`])
   }
 
   if (p.literal) {
-    return [`${p.target.table} as (select i.sourceKey, i.key, '${p.literal}' as value
-      from ${p.source.table} i)`]
+    return prerequisites.concat([`${p.target.table} as (select i.sourceKey, i.key, '${p.literal}' as value from ${p.source.table} i)`])
   }
 
   if (p.op === '~' || p.op === '=') {
-    // if (p.args[0].length === 1 && p.args[0][0].context) {
-    //   ret += "\nFN~ where i.value ilike '${JSON.stringify(p.args[1][0]})'"
-    // } else {
-    prerequisites = prerequisites.concat((p.args[0] || []).flatMap(a => queryPathItemToSql(a)))
-    prerequisites = prerequisites.concat((p.args[1] || []).flatMap(a => queryPathItemToSql(a)))
     return prerequisites.concat([`${p.target.table} as (select i.sourceKey, i.key, (i.value=o.value) as value
       from ${p.args[0].at(-1).target.table} i join ${p.args[1].at(-1).target?.table} o on (i.key=o.key)
     )`])
-    // }
   }
 
   if (p.op === 'exists') {
     const existsTable = p.args[0].at(-1).target.table;
-    prerequisites = prerequisites.concat((p.args[0] || []).flatMap(a => queryPathItemToSql(a)))
-    return prerequisites.concat(`
-
-    ${p.target.table}(sourceKey, key, value) as (
+    return prerequisites.concat(`${p.target.table}(sourceKey, key, value) as (
       select  i.* from ${p.source.table} i
       where exists(select 1 from ${existsTable} e where e.sourceKey=i.key  and e.value = 1)
-    )
-    
-    `)
+    )`)
   }
 
+  if (p.op === 'where') {
+    const whereTable = p.args[0].at(-1).target.table;
+    return prerequisites.concat(`
+     ${p.target.table}(sourceKey, key, value) as (
+      select  i.* from ${p.source.table} i
+      join ${whereTable} o on i.key=o.key where o.value=1)`)
+  }
 
   let key, sourceKey;
-  if (p.array) {
+  if (isForEach && p.array) {
     sourceKey = p.source.table.includes('_') ? `i.sourceKey` : `json_extract(i.${p.source.column}, '$.id')`
     key = (p.source.table.includes('_') ? `i.key` : `json_extract(i.${p.source.column}, '$.id')`)   + ` || '_' || o.path `;
   } else {
@@ -182,20 +181,15 @@ function queryPathItemToSql(p, isForEach) {
     sourceKey = "i.sourceKey"
   }
 
-  let ret = `${p.target.table}(sourceKey, key, value) as (select\n    ${sourceKey} as sourceKey,\n    ${key} as key,\n    `
-
+  let ret = `${p.target?.table}(sourceKey, key, value) as (select\n    ${sourceKey} as sourceKey,\n    ${key} as key,\n    `
   if (p.jsonPath && !p.array) {
-    ret += `json_extract('o.value', '${p.jsonPath}') `
+    ret += `json_extract(i.value, '${p.jsonPath}') `
   } else {
     ret += `o.value `
   }
-  ret += `from ${p.source.table} i `
+  ret += `from ${p.source?.table || JSON.stringify(p)} i `
   if (p.array) {
     ret += `join json_each(i.value, '${p.jsonPath}') o`
-  }
-  if (p.op === 'where') {
-    prerequisites = prerequisites.concat(p.args[0].flatMap(a => queryPathItemToSql(a)))
-    ret += `\n    FN.WHERE join ${p.args[0].at(-1).source.table})`
   }
   ret += "\n)"
   return prerequisites.concat([ret])
@@ -206,7 +200,7 @@ function queryAstToSql(ast) {
   if (ast.forEach) {
     ctes.push(
       ...ast.forEach.path
-        .flatMap((p, i) => queryPathItemToSql(p, i == ast.forEach.path.length - 1))
+        .flatMap((p, i) => queryPathItemToSql(p, true))
     )
   }
 
@@ -218,10 +212,13 @@ function queryAstToSql(ast) {
     ctes.push(...queryAstToSql(s))
   }
 
+  const completeSelectSources =  (ast.select || []).concat(ast.column || [])
+  let completeSelect = `\n    ${completeSelectSources[0].target.table} t0`
+  for (let i = 1; i < completeSelectSources.length; i++) {
+    completeSelect += `\n    join ${completeSelectSources[i].target.table} t${i} on (t${i-1}.key=t${i}.key)`
+  }
   ctes.push(
-    `${ast?.target?.table} as CROSS JOIN ALL ${(ast.select || [])
-      .concat(ast.column || [])
-      .map((s) => JSON.stringify(s?.target || null))}`,
+    `${ast?.target?.table} as (select * from ${completeSelect})`,
   )
   return ctes
 }
--- views.js
+++ views.js
@@ -3,12 +3,54 @@ const vd1 = {
   resource: 'Patient',
   select: [
     {
-      forEach: "contact.where(text='Josh')",
       column: [
         {
-          path: 'name.given',
-          name: 'given_name',
-          description: 'A single given name field with all names joined together.',
+          path: "name.given.where($this > 4)",
+          name: 'patient_j_name',
+        },
+      {
+          path: 'family2',
+          name: 'contact_family_name',
+        }
+      ],
+      select: [{
+        column: [{
+          path: "'inside_val'",
+          name: 'inside'
+        },{
+          path: 'family3',
+          name: 'contact_family_name',
+        }]
+
+      }]
+    }
+  ],
+}
+const vd3 = {
+  name: 'patient_demographics',
+  resource: 'Patient',
+  select: [
+    {
+      forEach: "name",
+      column: [
+        {
+          path: 'name.family',
+          name: 'patient_family_name',
+        },
+      ],
+      select: [{
+        forEach: "given",
+        column: [{
+          path: '$this',
+          name: 'patient_given'
+        }]
+      }]
+    },{
+      forEach: "contact.where(name.text='Josh')",
+      column: [
+        {
+          path: 'name.family',
+          name: 'contact_family_name',
         },
       ],
     },
